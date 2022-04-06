9baafa33d10a039c0e2382f7da3e287d54abc295
Arnau Mora <arnyminer.z@gmail.com>
2022-04-06 22:11:04 +0100

    :sparkles: Added development server
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

---
+++ index.js
@@ -0,0 +1,21 @@
+console.warn("Warning! Do not use in production. Testing server.");
+
+// Load parameters from .env file
+require('dotenv').config();
+
+const HTTP_PORT = process.env.HTTP_PORT || 3000;
+
+const express = require("express");
+const path = require('path');
+
+const app = express();
+
+// The opensheetmusicdisplay script
+app.use('/node_modules/opensheetmusicdisplay/build', express.static(path.join(__dirname, 'node_modules/opensheetmusicdisplay/build')));
+
+// Serve the html files
+app.use(express.static('html'))
+
+app.listen(HTTP_PORT, () => {
+    console.info(`Server available on: http://localhost:${HTTP_PORT}`);
+});
