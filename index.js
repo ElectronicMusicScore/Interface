a3ae1a54a98adc609a46a306b27c4526f3484aa7
Arnau Mora <arnyminer.z@gmail.com>
2022-04-07 01:01:06 +0100

    :sparkles: Added faked endpoints
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const HTTP_PORT = process.env.HTTP_PORT || 3000;
 const express = require("express");
 const path = require('path');
 
+const {faker} = require('@faker-js/faker');
+
 const app = express();
 
 // The opensheetmusicdisplay script
@@ -23,7 +25,22 @@ app.use(
 );
 
 // Serve the html files
-app.use(express.static('html'))
+app.use(express.static('html'));
+
+// Emulate the device endpoints
+app.get('/files', (req, res) => {
+    res.status(200).send({"files": ["/MozaVeilSample.xml"]});
+});
+app.get('/nets', (req, res) => {
+    let networks = [];
+
+    for (let j = 0; j < Math.floor(Math.random() * 20); j++)
+        networks.push({
+            ssid: faker.word.adjective() + ' ' + faker.word.noun(),
+        });
+
+    res.status(200).send({"networks": networks});
+});
 
 app.listen(HTTP_PORT, () => {
     console.info(`Server available on: http://localhost:${HTTP_PORT}`);
