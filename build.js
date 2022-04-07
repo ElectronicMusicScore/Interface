dbb767e67570121eae6786d1acb52a6621d9cdbb
Arnau Mora <arnyminer.z@gmail.com>
2022-04-07 12:10:07 +0100

    :sparkles: Improved compressor
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- build.js
+++ build.js
@@ -1,9 +1,22 @@
 const minify = require('html-minifier').minify;
 const purify = require('purify-css');
+const UglifyJS = require('uglify-js');
 
 const fs = require('fs');
 
+const {humanFileSize} = require('./utils');
+
 const purifyOptions = {minify: true};
+const uglifyjsOptions = {
+    compress: {
+        global_defs: {},
+        passes: 2
+    },
+    mangle: {
+        toplevel: true,
+    },
+    nameCache: {}
+};
 
 let initialHtml = fs.readFileSync('html/index.html').toString();
 let html = initialHtml;
@@ -32,55 +45,45 @@ initialHtml = initialHtml.replace(
     '<style>' + mainCssRaw + '</style>'
 );
 
-const osmdJs = fs.readFileSync('node_modules/opensheetmusicdisplay/build/opensheetmusicdisplay.min.js').toString();
-html = html.replace(
-    '<script src="/node_modules/opensheetmusicdisplay/build/opensheetmusicdisplay.min.js"></script>',
-    '<script>' + osmdJs + '</script>'
-);
-initialHtml = initialHtml.replace(
-    '<script src="/node_modules/opensheetmusicdisplay/build/opensheetmusicdisplay.min.js"></script>',
-    '<script>' + osmdJs + '</script>'
-);
+console.info(); // Leave line break
 
 const shortcutsJs = fs.readFileSync('html/shortcuts.js').toString();
-html = html.replace(
-    '<script src="shortcuts.js"></script>',
-    '<script>' + shortcutsJs + '</script>'
-);
-initialHtml = initialHtml.replace(
-    '<script src="shortcuts.js"></script>',
-    '<script>' + shortcutsJs + '</script>'
-);
-
-const mainJs = fs.readFileSync('html/main.js').toString();
-html = html.replace(
-    '<script src="main.js"></script>',
-    '<script>' + mainJs + '</script>'
-);
-initialHtml = initialHtml.replace(
-    '<script src="main.js"></script>',
-    '<script>' + mainJs + '</script>'
-);
-
 const modalJs = fs.readFileSync('html/modal.js').toString();
-html = html.replace(
-    '<script src="modal.js"></script>',
-    '<script>' + modalJs + '</script>'
-);
-initialHtml = initialHtml.replace(
-    '<script src="modal.js"></script>',
-    '<script>' + modalJs + '</script>'
-);
-
 const backendJs = fs.readFileSync('html/backend.js').toString();
+const mainJs = fs.readFileSync('html/main.js').toString();
+const osmdJsRaw = fs.readFileSync('node_modules/opensheetmusicdisplay/build/opensheetmusicdisplay.min.js').toString();
+const osmdJs = UglifyJS.minify({
+    'opensheetmusicdisplay.min.js': osmdJsRaw,
+    'shortcuts.js': shortcutsJs,
+    'main.js': mainJs,
+    'modal.js': modalJs,
+    'backend.js': backendJs,
+}, uglifyjsOptions).code;
 html = html.replace(
-    '<script src="backend.js"></script>',
-    '<script>' + backendJs + '</script>'
-);
+    '<script src="/node_modules/opensheetmusicdisplay/build/opensheetmusicdisplay.min.js"></script>',
+    '<script>' + osmdJs + '</script>'
+)
+    .replace('<script src="shortcuts.js"></script>', '')
+    .replace('<script src="main.js"></script>', '')
+    .replace('<script src="modal.js"></script>', '')
+    .replace('<script src="backend.js"></script>', '');
 initialHtml = initialHtml.replace(
+    '<script src="/node_modules/opensheetmusicdisplay/build/opensheetmusicdisplay.min.js"></script>',
+    '<script>' + osmdJsRaw + '</script>'
+).replace(
+    '<script src="main.js"></script>',
+    '<script>' + mainJs + '</script>'
+).replace(
+    '<script src="shortcuts.js"></script>',
+    '<script>' + shortcutsJs + '</script>'
+).replace(
+    '<script src="modal.js"></script>',
+    '<script>' + modalJs + '</script>'
+).replace(
     '<script src="backend.js"></script>',
     '<script>' + backendJs + '</script>'
 );
+console.info('JavaScript minifier:', (1 - (html.length / initialHtml.length)) * 100, '%');
 
 if (fs.existsSync('build'))
     fs.rmSync('build', {recursive: true, force: true});
@@ -91,8 +94,8 @@ const result = minify(html, {
     collapseBooleanAttributes: true,
     collapseInlineTagWhitespace: true,
     collapseWhitespace: true,
-    minifyCSS: {level: 2},
-    minifyJS: true,
+    minifyCSS: false,
+    minifyJS: false,
     removeAttributeQuotes: true,
     removeComments: true,
     removeEmptyAttributes: true,
@@ -103,11 +106,11 @@ const result = minify(html, {
     sortClassName: true,
     useShortDoctype: true,
 });
-fs.writeFile('build/index.html', result, err => {
+fs.writeFile('build/index.min.html', result, err => {
     if (err)
         return console.error(err);
     console.info();
-    console.info('Initial size:', initialHtml.length);
-    console.info("Minified size:", result.length);
+    console.info('Initial size: ', initialHtml.length, '(' + humanFileSize(initialHtml.length) + ')');
+    console.info('Minified size:', result.length, '(' + humanFileSize(result.length) + ')');
     console.info("---", ((1 - (result.length / initialHtml.length)) * 100), "% ---");
 });
