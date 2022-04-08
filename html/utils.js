014e44e86b60be0073db861542823ccf9fffe303
Arnau Mora <arnyminer.z@gmail.com>
2022-04-08 14:57:53 +0100

    :sparkles: Added `utils.js`
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

---
+++ html/utils.js
@@ -0,0 +1,27 @@
+/**
+ * Format bytes as human-readable text.
+ * @param size Number of bytes.
+ * @param si True to use metric (SI) units, aka powers of 1000. False to use
+ *           binary (IEC), aka powers of 1024.
+ * @param dp Number of decimal places to display.
+ * @return Formatted string.
+ */
+const humanFileSize = (size, si = false, dp = 1) => {
+    const thresh = si ? 1000 : 1024;
+
+    if (Math.abs(size) < thresh)
+        return size + ' B';
+
+    const units = si
+        ? ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+        : ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
+    let u = -1;
+    const r = 10 ** dp;
+
+    do {
+        size /= thresh;
+        ++u;
+    } while (Math.round(Math.abs(size) * r) / r >= thresh && u < units.length - 1);
+
+    return size.toFixed(dp) + ' ' + units[u];
+}
