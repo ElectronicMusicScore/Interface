3d2d5149a67f0f7c92df56c87b714955c0c78b50
Arnau Mora <arnyminer.z@gmail.com>
2022-04-11 16:20:46 +0100

    :sparkles: Compression of new files
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- build.js
+++ build.js
@@ -9,13 +9,12 @@ const {humanFileSize} = require('./utils');
 const purifyOptions = {minify: true};
 const uglifyjsOptions = {
     compress: {
-        global_defs: {},
-        passes: 2
+        passes: 2,
+        drop_console: true,
     },
     mangle: {
         toplevel: true,
     },
-    nameCache: {}
 };
 
 let initialHtml = fs.readFileSync('html/index.html').toString();
@@ -50,39 +49,54 @@ console.info(); // Leave line break
 const shortcutsJs = fs.readFileSync('html/shortcuts.js').toString();
 const modalJs = fs.readFileSync('html/modal.js').toString();
 const backendJs = fs.readFileSync('html/backend.js').toString();
+const filesBackendJs = fs.readFileSync('html/files-backend.js').toString();
+const downDetectorJs = fs.readFileSync('html/down-detector.js').toString();
+const utilsJs = fs.readFileSync('html/utils.js').toString();
 const mainJs = fs.readFileSync('html/main.js').toString();
 const osmdJsRaw = fs.readFileSync('node_modules/opensheetmusicdisplay/build/opensheetmusicdisplay.min.js').toString();
-const osmdJs = UglifyJS.minify({
+const chevronLeftSvg = fs.readFileSync('html/icons/chevron_left_black_24dp.svg').toString();
+const uploadFileSvg = fs.readFileSync('html/icons/upload_file_black_24dp.svg').toString();
+const zoomInSvg = fs.readFileSync('html/icons/zoom_in_black_24dp.svg').toString();
+const zoomOutSvg = fs.readFileSync('html/icons/zoom_out_black_24dp.svg').toString();
+const compressedJs = UglifyJS.minify({
     'opensheetmusicdisplay.min.js': osmdJsRaw,
     'shortcuts.js': shortcutsJs,
     'main.js': mainJs,
     'modal.js': modalJs,
+    'utils.js': utilsJs,
     'backend.js': backendJs,
-}, uglifyjsOptions).code;
+    'files-backend.js': filesBackendJs,
+    'down-detector.js': downDetectorJs,
+}, uglifyjsOptions);
+
+if (compressedJs.error)
+    return console.error(compressedJs.error);
+
 html = html.replace(
     '<script src="/node_modules/opensheetmusicdisplay/build/opensheetmusicdisplay.min.js"></script>',
-    '<script>' + osmdJs + '</script>'
+    '<script type="application/javascript" rel="script">' + compressedJs.code + '</script>'
 )
     .replace('<script src="shortcuts.js"></script>', '')
     .replace('<script src="main.js"></script>', '')
     .replace('<script src="modal.js"></script>', '')
-    .replace('<script src="backend.js"></script>', '');
+    .replace('<script src="backend.js"></script>', '')
+    .replace('<script src="files-backend.js"></script>', '')
+    .replace('<script src="down-detector.js"></script>', '')
+    .replace('<script src="utils.js"></script>', '')
+    .replaceAll('<img alt="..." src="icons/chevron_left_black_24dp.svg"/>', chevronLeftSvg)
+    .replaceAll('<img alt="..." src="icons/upload_file_black_24dp.svg"/>', uploadFileSvg)
+    .replaceAll('<img alt="..." src="icons/zoom_in_black_24dp.svg"/>', zoomInSvg)
+    .replaceAll('<img alt="..." src="icons/zoom_out_black_24dp.svg"/>', zoomOutSvg);
 initialHtml = initialHtml.replace(
     '<script src="/node_modules/opensheetmusicdisplay/build/opensheetmusicdisplay.min.js"></script>',
     '<script>' + osmdJsRaw + '</script>'
-).replace(
-    '<script src="main.js"></script>',
-    '<script>' + mainJs + '</script>'
-).replace(
-    '<script src="shortcuts.js"></script>',
-    '<script>' + shortcutsJs + '</script>'
-).replace(
-    '<script src="modal.js"></script>',
-    '<script>' + modalJs + '</script>'
-).replace(
-    '<script src="backend.js"></script>',
-    '<script>' + backendJs + '</script>'
-);
+).replace('<script src="main.js"></script>', '<script>' + mainJs + '</script>')
+    .replace('<script src="shortcuts.js"></script>', '<script>' + shortcutsJs + '</script>')
+    .replace('<script src="modal.js"></script>', '<script>' + modalJs + '</script>')
+    .replace('<script src="utils.js"></script>', '<script>' + utilsJs + '</script>')
+    .replace('<script src="backend.js"></script>', '<script>' + backendJs + '</script>')
+    .replace('<script src="files-backend.js"></script>', '<script>' + filesBackendJs + '</script>')
+    .replace('<script src="down-detector.js"></script>', '<script>' + downDetectorJs + '</script>');
 console.info('JavaScript minifier:', (1 - (html.length / initialHtml.length)) * 100, '%');
 
 if (fs.existsSync('build'))
